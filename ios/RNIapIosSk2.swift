947936c406c5940268be946d18bb42a91de50110
Charles Chen <ccchen920@gmail.com>
2025-08-27 09:59:23 -0700

    fix getting currency

--- ios/RNIapIosSk2.swift
+++ ios/RNIapIosSk2.swift
@@ -1080,9 +1080,9 @@ class RNIapIosSk2iOS15: Sk2Delegate {
     public func getStorefront(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
         Task {
             let storefront = await Storefront.current
-            let currency = Storefront.currency;
+            // let currency = Storefront.currency;
             // resolve(storefront?.countryCode)
-            resolve(["storefront": storefront, "currency": currency])
+            resolve(["storefront": storefront?.countryCode, "currency": storefront?.currency?.identifier])
         }
     }
 }
