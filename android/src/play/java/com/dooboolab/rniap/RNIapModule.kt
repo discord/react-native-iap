74f233ecb98b08581754693f0c6dcd37c145d9ae
Alex Hendrix <alex.hendrix@discordapp.com>
2025-10-08 16:12:34 -0500

    Android RN81 Support

--- android/src/play/java/com/dooboolab/rniap/RNIapModule.kt
+++ android/src/play/java/com/dooboolab/rniap/RNIapModule.kt
@@ -461,7 +461,7 @@ class RNIapModule(
         isOfferPersonalized: Boolean, // New parameter in V5
         promise: Promise,
     ) {
-        val activity = currentActivity
+        val activity = reactContext.currentActivity
         if (activity == null) {
             promise.safeReject(PromiseUtils.E_UNKNOWN, "getCurrentActivity returned null")
             return
